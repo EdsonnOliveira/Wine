b3254fc107439c164afea3ecc15901ac727f9bc0
EdsonnOliveira <72781591+EdsonnOliveira@users.noreply.github.com>
2022-03-05 22:34:57 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://170.83.160.85:8080/.new/api/wine/api/',
+    baseURL: 'http://localhost/wine/api/',
 })
 
 export default api;
